afcada0537cde6f7ad5710de3843dec551e79369
Aidan Nabass <ai.nabass@gmail.com>
2014-09-22 23:28:02 -0700

    README and more
    Added README and code cleanup

---
+++ resources/README.txt
@@ -0,0 +1,18 @@
+Welcome to 'Super Pang'
+A pong clone by Aidan Nabass
+
+Pang is played by 2 players with control of paddles. You get points by knocking the ball past the other players paddle.
+First to 10 points wins.
+The high score screen keeps track of the highest losing score.
+
+
+Controls
+++++++++++
+Player 1 (Left Side)
+ Move Up - 'W'
+ Move Down - 'S'
+
+Player 2 (Right Side)
+ Move Up - 'Up Arrow'
+ Move Down - 'Down Arrow'
+
--- source/main.cpp
+++ source/main.cpp
@@ -1,37 +1,81 @@
-﻿#include "AIE.h"
+﻿/*
+const char* GAME_NAME = Super Pang
+const char* NAME = Aidan Nabass
+const char* EMAIL = aidan.nabass@students.aie.edu.au
+
+//Description time:
+	Super Pang is a game created to be a clone of Pong.
+	With 2 paddles and 1 ball the game resembles table tennis; pitting 2 players against each other in an attempt to score a point on the other.
+	Player 1 is on the left and is controlled by the 'W' and 'S' keys.
+	Player 2 is on the right and is controlled by the 'Up_Arrow' and 'Down_Arrow' keys.
+	The main menu can be accessed at any time with 'F1'
+	The biggest loser screen can be accessed from the main menu with 'F2'
+*/
+
+#include "AIE.h"
 #include <iostream>
 #include <fstream>
 
 
-
+//Screen values
 int SCREEN_WIDTH = 800;
 int SCREEN_HEIGHT = 600;
 const float X_CENTER = SCREEN_WIDTH / 2;
 const float Y_CENTER = SCREEN_HEIGHT / 2;
+
+//Keycodes
+const int ESC_KEYCODE = 256;
+const int ENTER_KEYCODE = 257;
+const int F1_KEYCODE = 290;
+const int F2_KEYCODE = 291;
+const int UP_ARROW_KEYCODE = 265;
+const int DOWN_ARROW_KEYCODE = 264;
+const int MAX_CHAR_COUNT = 10;
+
+//Paddles
+const int PADDLE_SPEED = 225;
+
+//Main Menu
+const char* MAIN_MENU_START_TEXT = "Press <Enter> to Start";
+const char* MAIN_MENU_WELCOME = "Welcome to Super Pang!";
+const char* F1_TO_BIG_LOSER = "Press <F2> to check out biggest loser";
+const char* ESC_TO_QUIT = "Press <Esc> to Quit";
+
+//End Game Screen
+const char* PLAYER_1_WINS = "Player 1 wins!";
+const char* PLAYER_2_WINS = "Player 2 wins!";
+
+//High Score Menu
+const char* HIGH_SCORE_TEXT = "The biggest loser with a score of :";
+const char* HIGH_SCORE_LOG = "Biggestloser.pong";
+const char* HIGH_SCORE_TO_MAIN = "Press <F1> to go to Main Menu";
+//Game sprite location
 const char* SPRITE = "./images/purple.png";
 
-//Initialize ball speed
-float speed = 150.f;
-
 //Initialize highscore
-int highScore = 0;
+int highScore;
 
 //Initialize char buffer
-char buffer[10];
+char buffer[MAX_CHAR_COUNT];
 
 //Psuedo-Functions
 void UpdateMainMenu();
 void UpdateGamePlay(float a_deltaTime);
 void UpdateEndGame();
-void makeFile();
+void UpdateHighScore();
+void loadFile();
 void writeFile();
+void InitializeGameObjects();
 float GetLeft(float a_x, int a_width);
 float GetRight(float a_x, int a_width);
 float GetTop(float a_y, int a_height);
 float GetBottom(float a_y, int a_height);
 float GetDistance(float a_position1, float a_position2);
 
+//
 
+
+//Create paddle structure
 struct Paddle {
 	unsigned int spriteID;
 	float xPos;
@@ -49,11 +93,6 @@ struct Paddle {
 		height = a_height;
 	};
 
-	void SetBorder(unsigned int a_topBorder, unsigned int a_bottomBorder){
-		topBorder = a_topBorder;
-		bottomBorder = a_bottomBorder;
-	};
-
 	void SetMoveKeys(unsigned int a_moveUp, unsigned int a_moveDown){
 		moveUp = a_moveUp;
 		moveDown = a_moveDown;
@@ -64,21 +103,23 @@ struct Paddle {
 		yPos = a_y;
 	};
 
+	//Paddle movement and border collision
 	void Move(float a_deltaTime){
 		if (IsKeyDown(moveUp)){
 			if (yPos >= SCREEN_HEIGHT - (height / 2)){
 				yPos = SCREEN_HEIGHT - (height / 2);
 			}
-			yPos += a_deltaTime * 200.f;
+			yPos += a_deltaTime * PADDLE_SPEED;
 		}
 		if (IsKeyDown(moveDown)){
 			if (yPos <= (height / 2)){
 				yPos = (height / 2);
 			}
-			yPos -= a_deltaTime * 200.f;
+			yPos -= a_deltaTime * PADDLE_SPEED;
 		}
 	}
 
+	//Helper functions
 	float GetLeft(){
 		return xPos - (width / 2);
 	}
@@ -100,6 +141,7 @@ struct Paddle {
 Paddle player1;
 Paddle player2;
 
+//Create Ball structure
 struct Ball {
 	unsigned int spriteID;
 	float xPos;
@@ -119,9 +161,9 @@ struct Ball {
 		}
 	}
 
-	//Reset the ball position and increment score
+	//Reset the ball position when out of bounds
 	void Reset(){
-		if (xPos >= SCREEN_WIDTH || xPos <= 0){
+		if (xPos >= SCREEN_WIDTH || xPos <= 0 || yPos > SCREEN_HEIGHT || yPos < 0){
 			Score();
 			xPos = X_CENTER;
 			yPos = Y_CENTER;
@@ -129,12 +171,14 @@ struct Ball {
 		}
 	}
 
+	//Top-Bottom screen collision
 	void yScreenCollision(){
 		if (yPos >= SCREEN_HEIGHT - (height / 2) || yPos <= 0 + (height / 2)){
 			ySpeed *= -1;
 		}
 	}
 
+	//Helper functions
 	float GetLeft(){
 		return xPos - (width / 2);
 	}
@@ -163,6 +207,7 @@ enum GAMESTATES{
 	MAIN_MENU,
 	GAMEPLAY,
 	END,
+	HIGH_SCORE,
 };
 
 //Initialize Ball
@@ -173,33 +218,11 @@ GAMESTATES currentState = MAIN_MENU;
 
 int main(int argc, char* argv[])
 {
-	Initialise(SCREEN_WIDTH, SCREEN_HEIGHT, false, "My Awesome Game");
+	Initialise(SCREEN_WIDTH, SCREEN_HEIGHT, false, "Super Pang");
 
 	SetBackgroundColour(SColour(0, 0, 0, 255));
 
-	//Initialize paddle
-	player1.SetPostion(SCREEN_WIDTH * 0.1f, SCREEN_HEIGHT / 2);
-	player1.SetSize(28, 120);
-	player1.spriteID = CreateSprite(SPRITE, player1.width, player1.height, true);
-	player1.score = 0;
-	player1.SetMoveKeys('W', 'S');
-
-	//Initialize paddle 2
-	player2.SetPostion(SCREEN_WIDTH * 0.9f, SCREEN_HEIGHT / 2);
-	player2.SetSize(28, 120);
-	player2.spriteID = CreateSprite(SPRITE, player2.width, player2.height, true);
-	player2.score = 0;
-	player2.SetMoveKeys(265, 264);
-
-	//Initialize ball
-	ball.xPos = X_CENTER;
-	ball.yPos = Y_CENTER;
-	ball.width = 20;
-	ball.height = 20;
-	ball.xSpeed = speed;
-	ball.ySpeed = speed;
-	ball.spriteID = CreateSprite(SPRITE, ball.width, ball.height, true);
-
+	InitializeGameObjects();
 
 	//Game Loop
 	do
@@ -208,6 +231,8 @@ int main(int argc, char* argv[])
 		ClearScreen();
 		//Initialize time
 		float deltaT = GetDeltaTime();
+		//Load Highscores
+		loadFile();
 
 		switch (currentState){
 
@@ -224,6 +249,9 @@ int main(int argc, char* argv[])
 			UpdateEndGame();
 			break;
 
+		case HIGH_SCORE:
+			UpdateHighScore();
+
 		default:
 			break;
 		}
@@ -235,44 +263,72 @@ int main(int argc, char* argv[])
 	return 0;
 }
 void UpdateMainMenu(){
-	DrawString("Press Enter to Start!", X_CENTER - 120, Y_CENTER);
-	if (IsKeyDown(257)){
+	// Draw UI elements
+	DrawString(MAIN_MENU_WELCOME, X_CENTER - 150, Y_CENTER + 200);
+	DrawString(MAIN_MENU_START_TEXT, X_CENTER - 200, Y_CENTER);
+	DrawString(ESC_TO_QUIT, X_CENTER - 200, Y_CENTER - 40);
+	DrawString(F1_TO_BIG_LOSER, X_CENTER - 200, Y_CENTER - 80);
+	if (IsKeyDown(ENTER_KEYCODE)){
 		currentState = GAMEPLAY;
 	}
-	if (IsKeyDown(291)){
-		currentState = END;
+	if (IsKeyDown(F2_KEYCODE)){
+		currentState = HIGH_SCORE;
 	}
-	if (IsKeyDown(256)){
+	if (IsKeyDown(ESC_KEYCODE)){
 		Shutdown();
 		exit(0);
 	}
 
 }
 
-void UpdateEndGame(){
-	if (player1.score > player2.score){
-		highScore = player1.score;
-		DrawString("Player 1 Wins", X_CENTER - 100, Y_CENTER);
-	}
-	else{
-		highScore = player2.score;
-		DrawString("Player 2 Wins", X_CENTER - 100, Y_CENTER);
-	}
-	DrawString("Press <Esc> to quit!", X_CENTER - 140, Y_CENTER - 40);
-	DrawString("The highscore is currently:", X_CENTER - 190, Y_CENTER - 80);
-	DrawString(itoa(highScore,buffer,10), X_CENTER + 145, Y_CENTER - 80);
-
+void UpdateHighScore(){
 	
-	writeFile();
-	makeFile();
-
-	if (IsKeyDown(290)){
+	DrawString(HIGH_SCORE_TEXT, X_CENTER - 190, Y_CENTER);
+	DrawString(itoa(highScore, buffer, 10), X_CENTER + 225, Y_CENTER);
+	DrawString(HIGH_SCORE_TO_MAIN, X_CENTER - 190, Y_CENTER - 40);
+	
+	//Change to Main_Menu
+	if (IsKeyDown(F1_KEYCODE)){
 		currentState = MAIN_MENU;
 	}
 }
 
+void UpdateEndGame(){
+	if (player1.score > player2.score){
+		if (player2.score >= highScore){
+			highScore = player2.score;
+		}
+		DrawString(PLAYER_1_WINS, X_CENTER - 100, Y_CENTER);
+	}
+	else{
+		if (player1.score >= highScore){
+			highScore = player1.score;
+		}
+		DrawString(PLAYER_2_WINS, X_CENTER - 100, Y_CENTER);
+	}
+	DrawString(ESC_TO_QUIT, X_CENTER - 140, Y_CENTER - 40);
+	DrawString(HIGH_SCORE_TEXT, X_CENTER - 190, Y_CENTER - 80);
+	DrawString(itoa(highScore, buffer, 10), X_CENTER + 225, Y_CENTER - 80);
+	
+	//Track biggest loser
+	writeFile();
+	loadFile();
+
+	//Change to Main_Menu
+	if (IsKeyDown(F1_KEYCODE)){
+		currentState = MAIN_MENU;
+	}
+
+	//Exit game
+	if (IsKeyDown(ESC_KEYCODE)){
+		Shutdown();
+		exit(0);
+	}
+}
+
 void UpdateGamePlay(float a_deltaTime){
-	if (IsKeyDown(290)){
+	//Change to Main_Menu
+	if (IsKeyDown(F1_KEYCODE)){
 		currentState = MAIN_MENU;
 	}
 
@@ -296,7 +352,8 @@ void UpdateGamePlay(float a_deltaTime){
 	//Ball collision with top and bottom screen
 	ball.yScreenCollision();
 
-	//Ball X collision
+	//Ball X collision sides of paddle
+		//Player 2
 	if (ball.xPos - ball.xChange(a_deltaTime) < player2.GetLeft()){
 		if (ball.GetBottom() <= player2.GetTop() && ball.GetTop() >= player2.GetBottom()){
 			if (ball.GetRight() >= player2.GetLeft()){
@@ -305,6 +362,7 @@ void UpdateGamePlay(float a_deltaTime){
 			}
 		}
 	}
+		//Player 1
 	if (ball.xPos - ball.xChange(a_deltaTime) > player1.GetRight()){
 		if (ball.GetBottom() <= player1.GetTop() && ball.GetTop() >= player1.GetBottom()){
 			if (ball.GetLeft() <= player1.GetRight()){
@@ -314,7 +372,8 @@ void UpdateGamePlay(float a_deltaTime){
 		}
 	}
 
-	//Y Collision Top
+	//Y Collision top of paddle
+		//Player 2 Collision
 	if (ball.GetBottom() - ball.yChange(a_deltaTime) > player2.GetTop()){
 		if (ball.GetLeft() <= player2.GetRight() && ball.GetRight() >= player2.GetLeft()){
 			if (ball.GetBottom() <= player2.GetTop()){
@@ -323,6 +382,7 @@ void UpdateGamePlay(float a_deltaTime){
 			}
 		}
 	}
+		//Player 1 Collision
 	if (ball.GetBottom() - ball.yChange(a_deltaTime) > player1.GetTop()){
 		if (ball.GetLeft() <= player1.GetRight() && ball.GetRight() >= player1.GetLeft()){
 			if (ball.GetBottom() <= player1.GetTop()){
@@ -332,7 +392,8 @@ void UpdateGamePlay(float a_deltaTime){
 		}
 	}
 
-	//Y Collision Bottom
+	//Y Collision bottom of paddle
+		//Player 2 Collision
 	if (ball.GetTop() - ball.yChange(a_deltaTime) < player2.GetBottom()){
 		if (ball.GetLeft() <= player2.GetRight() && ball.GetRight() >= player2.GetLeft()){
 			if (ball.GetTop() >= player2.GetBottom()){
@@ -341,7 +402,7 @@ void UpdateGamePlay(float a_deltaTime){
 			}
 		}
 	}
-
+		//Player 1 Collision
 	if (ball.GetTop() - ball.yChange(a_deltaTime) < player1.GetBottom()){
 		if (ball.GetLeft() <= player1.GetRight() && ball.GetRight() >= player1.GetLeft()){
 			if (ball.GetTop() >= player1.GetBottom()){
@@ -365,18 +426,46 @@ void UpdateGamePlay(float a_deltaTime){
 
 void writeFile(){
 	std::fstream file;
-	file.open("Highscore.txt", std::ios::out);
+	file.open(HIGH_SCORE_LOG, std::ios::out);
 	file << highScore;
 	file.close();
 }
 
-void makeFile(){
+void loadFile(){
 
 	std::fstream file;
-	file.open("Highscore.txt", std::ios::in);
+	file.open(HIGH_SCORE_LOG, std::ios::in);
 
 	//character buffer
 	file.getline(buffer, 10);
 	highScore = atoi(buffer);
 	file.close();
+}
+
+void InitializeGameObjects(){
+	//Initialize speeds
+	float speed = 200.f;
+
+	//Initialize paddle
+	player1.SetPostion(SCREEN_WIDTH * 0.1f, SCREEN_HEIGHT / 2);
+	player1.SetSize(28, 120);
+	player1.spriteID = CreateSprite(SPRITE, player1.width, player1.height, true);
+	player1.score = 0;
+	player1.SetMoveKeys('W', 'S');
+
+	//Initialize paddle 2
+	player2.SetPostion(SCREEN_WIDTH * 0.9f, SCREEN_HEIGHT / 2);
+	player2.SetSize(28, 120);
+	player2.spriteID = CreateSprite(SPRITE, player2.width, player2.height, true);
+	player2.score = 0;
+	player2.SetMoveKeys(UP_ARROW_KEYCODE, DOWN_ARROW_KEYCODE);
+
+	//Initialize ball
+	ball.xPos = X_CENTER;
+	ball.yPos = Y_CENTER;
+	ball.width = 20;
+	ball.height = 20;
+	ball.xSpeed = speed;
+	ball.ySpeed = speed;
+	ball.spriteID = CreateSprite(SPRITE, ball.width, ball.height, true);
 }
